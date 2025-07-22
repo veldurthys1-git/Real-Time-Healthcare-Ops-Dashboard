2ca18a1890c07d9a65c25543839412f82fb48381
Charan <veldurthysaicharan5@gmail.com>
2025-07-21 19:07:07 -0700

    Initial commit with Vite project Final Commit with minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Real-Time-Healthcare-Ops-Dashboard/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
